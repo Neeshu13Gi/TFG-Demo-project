e63043f10555cf8af7871f976cc518d11e8f0646
Neeshu13Gi <neeshugautam8795@gmail.com>
2026-06-05 16:19:55 +0530

    Fixed Groq response format

--- webgl/ServiceWorker.js
+++ webgl/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Web_TFGPro-0.1.0";
 const contentToCache = [
-    "Build/TFGWeb(20-02-26).loader.js",
-    "Build/TFGWeb(20-02-26).framework.js",
-    "Build/TFGWeb(20-02-26).data",
-    "Build/TFGWeb(20-02-26).wasm",
+    "Build/CareerCoach.loader.js",
+    "Build/CareerCoach.framework.js",
+    "Build/CareerCoach.data",
+    "Build/CareerCoach.wasm",
     "TemplateData/style.css"
 
 ];
