ae55eeab156e06dd6c07ae33dc079190ec877333
Neeshu <132734073+Neeshu13Gi@users.noreply.github.com>
2026-07-24 22:28:10 +0530

    Add files via upload

---
+++ webgl/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Web_TFGPro-0.1.0";
+const contentToCache = [
+    "Build/TFGWeb(20-02-26).loader.js",
+    "Build/TFGWeb(20-02-26).framework.js",
+    "Build/TFGWeb(20-02-26).data",
+    "Build/TFGWeb(20-02-26).wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
